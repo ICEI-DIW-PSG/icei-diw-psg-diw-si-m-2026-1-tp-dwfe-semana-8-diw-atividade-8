f46022c7243ef2c8b8e226b07babc6874bdb1586
Isabela <ilrsilva@sga.pucminas.br>
2026-05-13 23:08:12 -0300

    inicio do catálogo

---
+++ public/script.js
@@ -0,0 +1,13 @@
+/* B1 - Definição de dados */
+
+const catalogo = [
+    {
+        id: 1,
+        titulo: "Smallville: As aventuras do superboy ", 
+        tipo: "serie",
+        ano: 2001, 
+        generos: ["drama", "ação"],
+        nota: 9.5,
+        assistido: true
+    }
+]
